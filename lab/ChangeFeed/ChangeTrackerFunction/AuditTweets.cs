a16004b82f194640d1cab1e35d2a5525010e19e1
Naseer Patel <naseer.patel@live.com>
2018-09-13 14:15:24 -0400

    Change Feed Processor added

---
+++ lab/ChangeFeed/ChangeTrackerFunction/AuditTweets.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace ChangeTrackerFunction
+{
+    public static class AuditTweets
+    {
+        [FunctionName("AuditTweets")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "TweetsDb",
+            collectionName: "Tweets",
+            ConnectionStringSetting = "DBConnection",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> documents, 
+            TraceWriter log)
+        {
+
+            if (documents != null && documents.Count > 0)
+            {
+                log.Verbose("Documents modified " + documents.Count);
+                log.Verbose("First document Id " + documents[0].Id);
+            }
+        }
+    }
+}
