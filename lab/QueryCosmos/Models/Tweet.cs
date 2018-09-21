4ff9bfe4bb05683cfc1897d21e99984f6213210d
Naseer Patel <naseer.patel@live.com>
2018-09-20 22:30:25 -0400

    Query Cosmos solution added.

---
+++ lab/QueryCosmos/Models/Tweet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.CosmosSQL.Models
+{
+    public class Tweet
+    {
+        public string Id { get;  set; }
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; }
+        public User User { get; set; }
+        public List<HashTag> HashTags { get; set; }
+        
+    }
+}
